7ba8676a054d759c85ebc733dd3ea9bee5b933e7
ericx2x <ericjlima@gmail.com>
2019-01-15 21:44:37 -0500

    Create asyncAwaitTest.js

---
+++ asyncAwaitTest.js
@@ -0,0 +1,28 @@
+https://www.youtube.com/watch?v=IGoAdn-e5II
+
+var person = "Eric Lima";
+setInterval(() => console.log(person + " have a good night.", 500));
+
+let busy_level = 0;
+async function foo(){
+  await new Promise((resolve, reject) => {
+    const doTurn = () => {
+      Math.sqrt(Math.random());
+      busy_level++;
+      if (Math.random() < 0.0000001){
+        console.log(busy_level);
+      }
+
+      setTimeout(doTurn,1000);
+    };
+
+    doTurn();
+
+  console.log("hmm");
+  resolve('ticket');
+  });
+}
+
+foo().then(() => {
+  console.log("done");
+});
